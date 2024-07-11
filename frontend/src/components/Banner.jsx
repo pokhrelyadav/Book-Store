d4d2cda5afbab6394d77551233f5988f7a4a6ac1
pokhrelyadav <pokhrelyadav412@gmail.com>
2024-07-11 20:41:30 +0545

    Added email handle for get started and usestate for email

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,7 +1,13 @@
-import React, { useEffect } from 'react';
-import banner from '/Banner.png';
+import React, { useEffect, useState } from "react";
+import banner from "/Banner.png";
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  const handleEmail = () => {
+    navigate("/signup", { state: { email: email } });
+  };
   return (
     <>
       <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex md:flex-row flex-col my-10">
@@ -14,10 +20,11 @@ const Banner = () => {
             <p className="text-xl">
               Welcome to our platform dedicated to expanding your knowledge and
               fostering curiosity. Dive into a wealth of resources curated to
-              inspire and empower your learning journey. Whether you're here to
-              explore cutting-edge technologies, master new skills, or deepen
-              your understanding of the world around us, you'll find everything
-              you need to fuel your passion for continuous growth.
+              inspire and empower your learning journey. Whether you&apose;re
+              here to explore cutting-edge technologies, master new skills, or
+              deepen your understanding of the world around us, you&apose;ll
+              find everything you need to fuel your passion for continuous
+              growth.
             </p>
             <label className="input input-bordered flex items-center gap-2 cursor-pointer">
               <svg
@@ -29,12 +36,17 @@ const Banner = () => {
                 <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                 <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
               </svg>
-              <input type="text" className="grow" placeholder="Email" />
+              <input
+                type="text"
+                className="grow"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
           </div>
-          <a href="#slider" className="mt-6 btn btn-secondary ">
+          <button onClick={handleEmail} className="mt-6 btn btn-secondary ">
             Get Started
-          </a>
+          </button>
         </div>
         <div className="order-1 md:order-2 h-full w-full md:w-1/2 flex justify-center  mt-[-50px]">
           <img src="/Banner1.png" className="h-92 h-92" alt="" />
